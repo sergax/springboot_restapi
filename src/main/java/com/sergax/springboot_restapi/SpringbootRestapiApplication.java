8430eca48accac54e30709bb91a80e55302a8ab1
sergax <serey.aksenchenko@gmail.com>
2022-04-17 10:22:25 +0300

    Initial commit

---
+++ src/main/java/com/sergax/springboot_restapi/SpringbootRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.sergax.springboot_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergax/springboot_restapi/SpringbootRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergax.springboot_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
